d6e16b4afa4f5843db503363c009104b2122fc63
Anatoly <apvnsk1994@gmai.com>
2022-01-04 15:09:46 +0700

    Updates

---
+++ precache-manifest.580746e6f90d21d26ae73d5469904ca0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed8d1a5f15f96fc2ef1f",
+    "url": "/qrme/static/css/main.022f62e7.chunk.css"
+  },
+  {
+    "revision": "ed8d1a5f15f96fc2ef1f",
+    "url": "/qrme/static/js/main.4257f469.chunk.js"
+  },
+  {
+    "revision": "d3121d1e64c1c1d10180",
+    "url": "/qrme/static/js/runtime~main.e16e5626.js"
+  },
+  {
+    "revision": "8992d2636184e2e37189",
+    "url": "/qrme/static/js/2.1a421962.chunk.js"
+  },
+  {
+    "revision": "983dd4d48b0e755b0ebf",
+    "url": "/qrme/static/js/3.5adc3d66.chunk.js"
+  },
+  {
+    "revision": "717a887a50fd3ad757785f95b720e8b6",
+    "url": "/qrme/index.html"
+  }
+];
